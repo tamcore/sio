b3e9143a4e82096783dc798f9610fbaa73daf4cc
Philipp Born <philipp@tamcore.eu>
2015-10-04 14:17:26 +0200

    add sample config

---
+++ config.sample.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DB_TYPE', 'sqlite');     // for now only sqlite supported. mysql comes later
+define('DB_USER', '');           // leave empty for sqlite
+define('DB_PASS', '');           // leave empty for sqlite
+define('DB_NAME', '');           // set to path for sqlite .db file
+
+define('ALLOW_REGISTER', false); // true or false if you'd allow other people to use your instance
+
+?>
