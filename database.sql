909df191e13ffd708eb5c43a3d2c72cd6de0f427
TamCore <philipp@tamcore.eu>
2015-12-31 22:11:33 +0100

    some sql cleanups..

--- database.sql
+++ database.sql
@@ -1,23 +1,26 @@
 CREATE TABLE `users` (
-    `id`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     `email` TEXT NOT NULL UNIQUE,
-    `password`  TEXT NOT NULL
-);
+    `password` TEXT NOT NULL
+    );
+
 CREATE TABLE `numbers` (
-    `id`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    `number`    NUMERIC NOT NULL UNIQUE,
-    `user`  INTEGER NOT NULL,
-    `dnd`   INTEGER NOT NULL DEFAULT 0,
-    `dndaction`    TEXT NOT NULL DEFAULT 'hangup'
-);
-CREATE TABLE `actions` (
-    `id`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    `number`    INTEGER NOT NULL,
-    `extnumber` TEXT NOT NULL,
-    `direction` TEXT NOT NULL,
-    `action`    TEXT NOT NULL,
-    `param1`    TEXT,
-    `param2`    TEXT,
-    `active`    INTEGER NOT NULL DEFAULT 0,
-    `type`      INTEGER NOT NULL DEFAULT 0
-);
+    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    `number` NUMERIC NOT NULL UNIQUE,
+    `user` INTEGER NOT NULL,
+    `dnd` INTEGER NOT NULL DEFAULT 0,
+    'dnd_action' TEXT NOT NULL DEFAULT 'hangup'
+    );
+
+CREATE TABLE 'actions' (
+    `id`INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    `number`INTEGER NOT NULL,
+    `extnumber`TEXT NOT NULL,
+    `direction`TEXT NOT NULL,
+    `action`TEXT NOT NULL,
+    `param1`TEXT,
+    `param2`TEXT,
+    'comment' TEXT,
+    'type' INTEGER DEFAULT 0,
+    'active' INTEGER NOT NULL DEFAULT 0
+    );
