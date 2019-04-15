09d4f2e546d24668d1a6f4bfc0732425860ff955
Philipp Born <philipp@tamcore.eu>
2019-04-15 11:30:07 +0200

    ignore user voicemail

--- include/api.class.php
+++ include/api.class.php
@@ -83,6 +83,8 @@ class api {
   public function isApiCall() {
     if (isset($_POST['from'], $_POST['to'], $_POST['direction']) == false)
       return false;
+    if (isset($_POST['user'][0]) AND $_POST['user'][0] == 'voicemail')
+      return false;
     if ($_POST['direction'] != 'in' AND $_POST['direction'] != 'out')
       return false;
     if (is_numeric($_POST['from']) == false OR is_numeric($_POST['to']) == false)
